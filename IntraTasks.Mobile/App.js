681ccbb68e50a47a01baec13b57cd753f0eec72a
Moisés Mariano <moisesduartem@gmail.com>
2021-05-16 22:09:30 -0300

    Configura react navigation & drawer

--- IntraTasks.Mobile/App.js
+++ IntraTasks.Mobile/App.js
@@ -1,28 +1,38 @@
 import React from 'react';
-import {View} from 'react-native';
-import {Header} from 'react-native-elements';
+import 'react-native-gesture-handler';
+
+import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+
+import {Header} from 'react-native-elements';
 import {TarefasScreen} from './src/screens/Tarefas';
+import {MembrosScreen} from './src/screens/Membros';
+
+const Drawer = createDrawerNavigator();
 
 function App() {
   return (
-    <SafeAreaProvider>
-      <View style={styles.view}>
+    <NavigationContainer>
+      <SafeAreaProvider>
         <Header
           leftComponent={{icon: 'menu', color: '#fff'}}
-          centerComponent={{text: 'Tarefas', style: {color: '#fff'}}}
-          rightComponent={{icon: 'add', color: '#fff'}}
+          centerComponent={{text: 'IntraTasks', style: {color: '#fff'}}}
         />
-        <TarefasScreen />
-      </View>
-    </SafeAreaProvider>
+        <Drawer.Navigator
+          sceneContainerStyle={styles.contentContainer}
+          initialRouteName="Tarefas">
+          <Drawer.Screen name="Tarefas" component={TarefasScreen} />
+          <Drawer.Screen name="Membros" component={MembrosScreen} />
+        </Drawer.Navigator>
+      </SafeAreaProvider>
+    </NavigationContainer>
   );
 }
 
 const styles = {
-  view: {
-    flex: 1,
-    backgroundColor: '#ffffff',
+  contentContainer: {
+    padding: 18,
   },
 };
 
