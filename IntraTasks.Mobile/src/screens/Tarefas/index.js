c748401b890f342b2f3151b6f47f31de68eee257
Moisés Mariano <moisesduartem@gmail.com>
2021-05-16 23:03:50 -0300

    Lista tarefas (hardcode)

--- IntraTasks.Mobile/src/screens/Tarefas/index.js
+++ IntraTasks.Mobile/src/screens/Tarefas/index.js
@@ -1,12 +1,97 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+
+import {ScrollView, Text, View} from 'react-native';
+import {colors, FAB, ListItem} from 'react-native-elements';
+import {Icon} from 'react-native-elements';
+
+import {situacoes, tarefas} from '../../services/data/mocked';
+
+function getSituacaoById(id) {
+  return situacoes.filter((el, i) => el.id === id)[0];
+}
 
 function TarefasScreen() {
+  const buttonsSize = 15;
+
   return (
-    <View>
-      <Text>Tarefas View</Text>
-    </View>
+    <ScrollView>
+      {tarefas.map((e, i) => (
+        <ListItem key={i} style={styles.listItem} bottomDivider>
+          <View style={styles.card}>
+            <View style={styles.cardBody}>
+              <Text style={styles.taskTitle}>{e.titulo}</Text>
+              <Text style={styles.situacao(e)}>
+                {getSituacaoById(e.situacaoId).nome}
+              </Text>
+              {e.observacao && (
+                <View style={styles.obsContainer}>
+                  <Text style={styles.obsText}>Observação</Text>
+                  <Text>{e.observacao}</Text>
+                </View>
+              )}
+            </View>
+            <View style={styles.itemOptions}>
+              <FAB
+                icon={
+                  <Icon
+                    color="white"
+                    size={buttonsSize}
+                    name="trash"
+                    type="font-awesome"
+                  />
+                }
+                size="small"
+                style={styles.fab}
+                color={colors.error}
+              />
+              <FAB
+                icon={
+                  <Icon
+                    color="white"
+                    size={buttonsSize}
+                    name="pencil"
+                    type="font-awesome"
+                  />
+                }
+                size="small"
+                style={styles.fab}
+                color={colors.primary}
+              />
+              <FAB
+                icon={
+                  <Icon
+                    color="white"
+                    size={buttonsSize}
+                    name="eye"
+                    type="font-awesome"
+                  />
+                }
+                size="small"
+                style={styles.fab}
+                color={colors.black}
+              />
+            </View>
+          </View>
+        </ListItem>
+      ))}
+    </ScrollView>
   );
 }
+
+const styles = {
+  taskTitle: {fontSize: 20},
+  card: {padding: 5},
+  cardBody: {paddingVertical: 10},
+  itemOptions: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    width: '100%',
+  },
+  obsContainer: {paddingVertical: 10},
+  obsText: {fontSize: 16, fontWeight: '700'},
+  listItem: {marginVertical: 5},
+  fab: {marginHorizontal: 5},
+  situacao: e => ({color: e.situacaoId === 1 ? colors.error : colors.success}),
+};
 
 export {TarefasScreen};
---
+++ IntraTasks.Mobile/src/services/data/mocked.js
@@ -0,0 +1,35 @@
+export const situacoes = [
+  {id: 1, nome: 'Pendente'},
+  {id: 2, nome: 'Feito'},
+];
+
+export const tarefas = [
+  {
+    id: 1,
+    titulo: 'Lavar a louça',
+    situacaoId: 1,
+  },
+  {
+    id: 2,
+    titulo: 'Lavar a louça',
+    observacao: 'Pratos e talheres do almoço',
+    situacaoId: 2,
+  },
+  {
+    id: 3,
+    titulo: 'Lavar a louça',
+    observacao: 'Pratos e talheres do almoço',
+    situacaoId: 2,
+  },
+  {
+    id: 4,
+    titulo: 'Lavar a louça',
+    situacaoId: 1,
+  },
+  {
+    id: 5,
+    titulo: 'Lavar a louça',
+    observacao: 'Pratos e talheres do almoço',
+    situacaoId: 1,
+  },
+];
