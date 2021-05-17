8c9a9c94365d891c967ff64d541bfd18904135cf
Moisés Mariano <moisesduartem@gmail.com>
2021-05-16 21:31:45 -0300

    Configura react native elements

---
+++ IntraTasks.Mobile/src/screens/Membros/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function MembrosScreen() {
+  return (
+    <View>
+      <Text>Membros View</Text>
+    </View>
+  );
+}
+
+export {MembrosScreen};
---
+++ IntraTasks.Mobile/src/screens/Tarefas/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function TarefasScreen() {
+  return (
+    <View>
+      <Text>Tarefas View</Text>
+    </View>
+  );
+}
+
+export {TarefasScreen};
