fa845286d1d8d9bcb354c390e3f19bd4dc29b39f
Moisés Mariano <moisesduartem@gmail.com>
2021-05-19 19:25:00 -0300

    Cria repositórios p/ Membro e Tarefa

---
+++ IntraTasks.Api/IntraTasks.BusinessLogic/Repository/MembroRepository.cs
@@ -0,0 +1,12 @@
+﻿using IntraTasks.DataAccess.Context;
+using IntraTasks.DataAccess.Domain;
+using IntraTasks.DataAccess.Repository;
+
+namespace IntraTasks.BusinessLogic.Repository
+{
+    public class MembroRepository : Repository<Membro>, IMembroRepository
+    {
+        public MembroRepository(AppDbContext context) : base(context)
+        { }
+    }
+}
---
+++ IntraTasks.Api/IntraTasks.BusinessLogic/Repository/TarefaRepository.cs
@@ -0,0 +1,19 @@
+﻿using IntraTasks.DataAccess.Context;
+using IntraTasks.DataAccess.Domain;
+using IntraTasks.DataAccess.Repository;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IntraTasks.BusinessLogic.Repository
+{
+    public class TarefaRepository : Repository<Tarefa>, ITarefaRepository
+    {
+        public TarefaRepository(AppDbContext context) : base(context)
+        {}
+
+        public IEnumerable<Tarefa> GetTarefaByAutorId(int autorId)
+        {
+            return Get().Where(tarefa => tarefa.AutorId == autorId);
+        }
+    }
+}
