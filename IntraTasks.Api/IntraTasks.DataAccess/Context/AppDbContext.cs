80471c7e207abf7469d1000a20098847c2f4b27d
Moisés Mariano <moisesduartem@gmail.com>
2021-05-19 19:09:01 -0300

    Cria IRepository e implementa em classe genérica

---
+++ IntraTasks.Api/IntraTasks.DataAccess/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using IntraTasks.DataAccess.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntraTasks.DataAccess.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<Membro> Membros { get; set; }
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
---
+++ IntraTasks.Api/IntraTasks.DataAccess/Repository/Repository.cs
@@ -0,0 +1,44 @@
+﻿using IntraTasks.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace IntraTasks.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected AppDbContext _context;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public IQueryable<T> Get()
+        {
+            return _context.Set<T>().AsNoTracking();
+        }
+
+        public T GetById(Expression<Func<T, bool>> predicate)
+        {
+            return _context.Set<T>().FirstOrDefault(predicate);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.Update(entity);
+        }
+    }
+}
